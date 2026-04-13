a95b48e2914ce9d1f1d2ba244bc9059cacd2d140
Elias Andersen <eoan24@student.aau.dk>
2026-04-13 09:39:42 +0200

    3 Simple tests

--- test/test_preproc.py
+++ test/test_preproc.py
@@ -1,14 +1,70 @@
+import pytest
+
 from Preprocessor.prepro import preprocessor
 
-import pytest
+INPUT_FILES = "test/input_files/"
+OUTPUT_FILES = "test/output_files/"
 
 def test_comments():
     # Arrange
     expected = "x in int := 1 @NEWLINE\n$"
-    output_file = "test/output_files/rasmus.gsl"
+    input_file = INPUT_FILES + "comments.gsl"
+    output_file = OUTPUT_FILES + "comments_out.gsl"
 
     # Act
-    preprocessor(inp="test/input_files/comments.gsl", outp=output_file)
+    preprocessor(input_file, output_file)
+
+    data = ""
+    with open(output_file, "r") as out:
+        data = out.read()
+
+    # Assert
+    assert expected == data, f"expected: {expected} actual: {data}"
+
+def test_indents():
+    # Arrange
+    expected = \
+    """graph G @NEWLINE
+@INDENT node a, b, c @NEWLINE
+a --> c weight integer 3 @NEWLINE
+@DEDENT repeat || G . nodes || - 1 times @NEWLINE
+@INDENT for each x1 <-- x2 with weight w in G @NEWLINE
+@INDENT relax (x1, x2, w) @NEWLINE
+@DEDENT for each x1 --> x2 with weight w in G @NEWLINE
+@INDENT if w > 5 then @NEWLINE
+@INDENT return false @NEWLINE
+@DEDENT return true @NEWLINE
+@DEDENT @DEDENT $"""
+
+    input_file = INPUT_FILES + "indents.gsl"
+    output_file = OUTPUT_FILES + "comments_out.gsl"
+
+    # Act
+    preprocessor(input_file, output_file)
+
+    data = ""
+    with open(output_file, "r") as out:
+        data = out.read()
+
+    # Assert
+    assert expected == data, f"expected: {expected} actual: {data}"
+
+def test_newlines():
+    # Arrange
+    expected = \
+    """x in int := 4 @NEWLINE
+repeat x times @NEWLINE
+@INDENT x := x + 1 @NEWLINE
+@DEDENT y in real := 3.14 @NEWLINE
+v in real := 2 @NEWLINE
+o in real := y^2 * v @NEWLINE
+$"""
+
+    input_file = INPUT_FILES + "newlines.gsl"
+    output_file = OUTPUT_FILES + "newlines_out.gsl"
+
+    # Act
+    preprocessor(input_file, output_file)
 
     data = ""
     with open(output_file, "r") as out:
