6a4c854aa2ab3dc65a4066cf75f69cecbe19d2a3
Zhdun12 <spongo123456@gmail.com>
2026-04-21 09:58:32 +0200

    Updated to latest naming convention.

--- Test/test_parser.py
+++ Test/test_parser.py
@@ -3,19 +3,19 @@ import pytest
 import subprocess # Used to run the generated parser
 import filecmp # Used to compare parsed output and correct output
 
-from prepro import preprocessor
+from test.prepro import preprocessor
 
 # Current naming uses parse tree, as this test is 
 # not adapted to an AST as output from parser.
 
-PARSED_OUTPUT_FILE_NAME = "Test/parsedInput.xml"
+PARSED_OUTPUT_FILE_NAME = "test/parsed_input.xml"
 
-def programParser(inputProgram: str, expectedOutput: str):
+def program_parser(inputProgram: str, expectedOutput: str):
     # Run the preprocessor on GSL input
     preprocessor(inputProgram, PARSED_OUTPUT_FILE_NAME)
 
     # Run the GSL parser as a subprocess and capture the output in the console
-    x = subprocess.run(["python3", "./gslParser.py", "-i", PARSED_OUTPUT_FILE_NAME], capture_output=True)
+    x = subprocess.run(["python3", "./gsl_parser.py", "-i", PARSED_OUTPUT_FILE_NAME], capture_output=True)
 
 
     # Debug print
@@ -34,15 +34,15 @@ def programParser(inputProgram: str, expectedOutput: str):
 
 # Expected output could be CorrectParseTrees/CorrectBellmanFOrd.xml
 def test_parser():
-    testProgramNames = ["Test/GslPrograms/bellmanFord.gsl", "Test/GslPrograms/simpleGraph.gsl"]
-    correctParseTreeName = ["Test/CorrectParseTrees/correctBellmanFord.xml", "Test/CorrectParseTrees/correctSimpleGraph.xml"]
+    test_program_names = ["test/gsl_programs/bellman_ford.gsl", "test/gsl_programs/simple_graph.gsl"]
+    correct_parse_tree_name = ["test/correct_parse_trees/correct_bellman_ford.xml", "test/correct_parse_trees/correct_simple_graph.xml"]
     results = []
     
-    for idx, fname in enumerate(testProgramNames):
+    for idx, fname in enumerate(test_program_names):
         print(fname)
-        print(correctParseTreeName[idx])
-        results.append((fname, programParser(fname, correctParseTreeName[idx])))
+        print(correct_parse_tree_name[idx])
+        results.append((fname, program_parser(fname, correct_parse_tree_name[idx])))
         
     print("\n")
-    for gslINput, testResult in results:
-        print(gslINput + ": " + str(testResult))
+    for gsl_input, test_result in results:
+        print(gsl_input + ": " + str(test_result))
