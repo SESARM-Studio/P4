ea758df38489f20ea561fe722425a3e921de448d
Zhdun12 <spongo123456@gmail.com>
2026-04-21 10:34:53 +0200

    Updated test, it now works pytest.

--- Test/test_parser.py
+++ Test/test_parser.py
@@ -8,14 +8,19 @@ from test.prepro import preprocessor
 # Current naming uses parse tree, as this test is 
 # not adapted to an AST as output from parser.
 
-PARSED_OUTPUT_FILE_NAME = "test/parsed_input.xml"
+program_names = ["test/gsl_programs/bellman_ford.gsl", "test/gsl_programs/simple_graph.gsl"]
+correct_parse_tree_name = ["test/correct_parse_trees/correct_bellman_ford.xml", "test/correct_parse_trees/correct_simple_graph.xml"]
 
-def program_parser(inputProgram: str, expectedOutput: str):
+def test_bellman_ford_parser():
+    parsed_bellman_ford_file = "test/parsed_programs/parsed_bellman_ford.xml"
+    input_program = program_names[0]
+    expected_output = correct_parse_tree_name[0]
+
     # Run the preprocessor on GSL input
-    preprocessor(inputProgram, PARSED_OUTPUT_FILE_NAME)
+    preprocessor(input_program, parsed_bellman_ford_file)
 
     # Run the GSL parser as a subprocess and capture the output in the console
-    x = subprocess.run(["python3", "./gsl_parser.py", "-i", PARSED_OUTPUT_FILE_NAME], capture_output=True)
+    x = subprocess.run(["python3", "./gsl_parser.py", "-i", parsed_bellman_ford_file], capture_output=True)
 
 
     # Debug print
@@ -23,26 +28,33 @@ def program_parser(inputProgram: str, expectedOutput: str):
 
 
     # Write produced AST to a file
-    with open(PARSED_OUTPUT_FILE_NAME, "w") as output:
+    with open(parsed_bellman_ford_file, "w") as output:
       output.write(str(x.stdout))
 
     # Compare parsed GSL program to expected correct parse tree
-    rc = filecmp.cmp(PARSED_OUTPUT_FILE_NAME, expectedOutput, shallow=False)
+    rc = filecmp.cmp(parsed_bellman_ford_file, expected_output, shallow=False)
     assert rc == True
 
-    return rc
+def test_simple_graph_parser():
+    parsed_simple_graph_file = "test/parsed_programs/parsed_simple_graph.xml"
+    input_program = program_names[1]
+    expected_output = correct_parse_tree_name[1]
 
-# Expected output could be CorrectParseTrees/CorrectBellmanFOrd.xml
-def test_parser():
-    test_program_names = ["test/gsl_programs/bellman_ford.gsl", "test/gsl_programs/simple_graph.gsl"]
-    correct_parse_tree_name = ["test/correct_parse_trees/correct_bellman_ford.xml", "test/correct_parse_trees/correct_simple_graph.xml"]
-    results = []
-    
-    for idx, fname in enumerate(test_program_names):
-        print(fname)
-        print(correct_parse_tree_name[idx])
-        results.append((fname, program_parser(fname, correct_parse_tree_name[idx])))
-        
-    print("\n")
-    for gsl_input, test_result in results:
-        print(gsl_input + ": " + str(test_result))
+    # Run the preprocessor on GSL input
+    preprocessor(input_program, parsed_simple_graph_file)
+
+    # Run the GSL parser as a subprocess and capture the output in the console
+    x = subprocess.run(["python3", "./gsl_parser.py", "-i", parsed_simple_graph_file], capture_output=True)
+
+
+    # Debug print
+    # print(x.stdout)
+
+
+    # Write produced AST to a file
+    with open(parsed_simple_graph_file, "w") as output:
+      output.write(str(x.stdout))
+
+    # Compare parsed GSL program to expected correct parse tree
+    rc = filecmp.cmp(parsed_simple_graph_file, expected_output, shallow=False)
+    assert rc == True
