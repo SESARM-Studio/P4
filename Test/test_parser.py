c1273837bafff1e3f11a82e142e178bbcc59efc6
Zhdun12 <spongo123456@gmail.com>
2026-04-21 10:49:00 +0200

    Changed preprocessor folder name to snake case (PEP8).

--- Test/test_parser.py
+++ Test/test_parser.py
@@ -1,7 +1,7 @@
 import subprocess # Used to run the generated parser
 import filecmp # Used to compare parsed output and correct output
 
-from Preprocessor.prepro import preprocessor
+from preprocessor.prepro import preprocessor
 
 # Current naming uses parse tree, as this test is 
 # not adapted to an AST as output from parser.
