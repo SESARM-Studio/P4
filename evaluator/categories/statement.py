c9cf82bb85fa04d88569c0313f7e0df5e1e5885b
Ramojuul <ls47ul@student.aau.dk>
2026-05-06 15:01:10 +0200

    merge

--- evaluator/categories/statement.py
+++ evaluator/categories/statement.py
@@ -26,11 +26,7 @@ def execute_statement(node: ASTNode, env_graph, env_var, env_algo, loc, graph_ob
         case GraphDecl():
             pass
         case Expression():
-<<<<<<< HEAD
-            return execute_expression(node, env_graph, env_var, env_algo, loc, graph_object, store)
-=======
-            v,exp_store = execute_expression(node, env_graph, env_var, env_algo, loc, graph_object, store)
->>>>>>> 4ed91a63d132ddb31586243cfc0056964b1f6a9a
+            return v,exp_store = execute_expression(node, env_graph, env_var, env_algo, loc, graph_object, store)
 
         case EdgeDecl():
             pass
