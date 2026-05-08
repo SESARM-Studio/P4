3fc3b2979c8eccb6a95fe42db43b53456ffdc0af
Zhdun12 <spongo123456@gmail.com>
2026-05-08 13:47:59 +0200

    Display, return implemented. Fixed circular imports. Added default case to notice case mismatch.

--- evaluator/categories/statement.py
+++ evaluator/categories/statement.py
@@ -1,12 +1,14 @@
 from parser.ast_builder import *
 from copy import deepcopy
 
-from . import *
+import evaluator.categories.algorithm
+import evaluator.categories.expression
+import evaluator.categories.declaration
 
 def execute_statement(node: ASTNode, loc, graph_object, store, env_var, env_algo, env_graph):
     match node:
         case Declaration():
-            ret = execute_declaration(node, env_graph, env_var, env_algo, loc, graph_object, store)
+            ret = evaluator.categories.declaration.execute_declaration(node, env_graph, env_var, env_algo, loc, graph_object, store)
             return ret.store, ret.env_var, env_algo, env_graph, None, ret.location
         case DeclarationInit():
             pass
@@ -14,7 +16,7 @@ def execute_statement(node: ASTNode, loc, graph_object, store, env_var, env_algo
             pass
         case IfStatement():
             copy_store = deepcopy(store)
-            expression, copy_store = execute_expression(node.condition, loc, graph_object, store, env_var, env_algo, env_graph) 
+            expression, copy_store = evaluator.categories.expression.execute_expression(node.condition, loc, graph_object, store, env_var, env_algo, env_graph) 
             match expression:
                 case True:
                     for i in node.then_statements:
@@ -46,24 +48,35 @@ def execute_statement(node: ASTNode, loc, graph_object, store, env_var, env_algo
         case RepeatStatement():
             pass
         case Algorithm():
-            ph_env_algo = execute_algorithm(node, env_graph, env_var, env_algo, loc, graph_object, store)
+            ph_env_algo = evaluator.categories.algorithm.execute_algorithm(node, env_graph, env_var, env_algo, loc, graph_object, store)
             return store, env_var, ph_env_algo, env_graph, None, loc
+        
+        case Expression():
+            exp_store = evaluator.categories.expression.execute_expression(node, env_graph, env_var, env_algo, loc, graph_object, store)
+            return store, env_var, env_algo, env_graph, None, loc
+        
         case GraphDecl():
             pass
         case Expression():
-            exp_store = execute_expression(node, env_graph, env_var, env_algo, loc, graph_object, store)
+            v,exp_store = evaluator.categories.expression.execute_expression(node, env_graph, env_var, env_algo, loc, graph_object, store)
             return exp_store, env_var, env_algo, env_graph, None, loc
 
         case EdgeDecl():
             pass
         case NodeDecl():
-            ret = execute_declaration(node, env_graph, env_var, env_algo, loc, graph_object, store)
+            ret = evaluator.categories.execute_declaration(node, env_graph, env_var, env_algo, loc, graph_object, store)
             return ret.store, ret.env_var, env_algo, env_graph, None, ret.location
         case GraphStatement():
             pass
         case LoopModifier():
             pass
         case DisplayStatement():
-            pass
+            v,exp_store = evaluator.categories.expression.execute_expression(node.expression, env_graph, env_var, env_algo, loc, graph_object, store)
+            print(v)
+            return exp_store, env_var, env_algo, env_graph, None, loc
         case ReturnStatement():
-            pass
+            v,exp_store = evaluator.categories.expression.execute_expression(node.expression, env_graph, env_var, env_algo, loc, graph_object, store)
+            return exp_store, env_var, env_algo, env_graph, v, loc
+        case _:
+            print("Error: No statement case match!")
+            return store, env_var, env_algo, env_graph, None, loc
