097e395bc5cba8ab4f7b2c7c3f4127d4df5c25dd
Elias Andersen <eoan24@student.aau.dk>
2026-05-11 11:21:32 +0200

    updated to new scoped type environments + tests

---
+++ tests/typesystem_unit_tests/test_scoped_type_env.py
@@ -0,0 +1,91 @@
+from typesystem.type_env import ScopedTypeEnvironment
+from typesystem.data_types import TypeEnum
+
+def test_scoped_type_env_binding_found_in_outer_scope():
+    # Arrange
+    expected = TypeEnum.REAL
+    env = ScopedTypeEnvironment()
+
+    # Act
+    env.current_scope = env.current_scope.bind("weight_sum", TypeEnum.REAL)
+
+    env = env.enter_scope()
+    env.current_scope = env.current_scope.bind("new_sum", TypeEnum.REAL)
+    env.current_scope = env.current_scope.bind("chromatic_color", TypeEnum.TEXT)
+    env.current_scope = env.current_scope.bind("Trie", TypeEnum.TREE)
+
+    actual = env.lookup("weight_sum")
+
+    # Assert
+    assert actual is not TypeEnum.UNKNOWN, "Binding not found"
+    assert actual == expected, f"actual -> {actual.name} == {expected.name} <- expected"
+
+def test_scoped_type_env_scope_destroyed():
+    # Arrange
+    expected = None
+    env = ScopedTypeEnvironment()
+
+    # Act
+    env = env.enter_scope()
+    env.current_scope = env.current_scope.bind("graph_num", TypeEnum.NAT)
+    env = env.exit_scope()
+
+    actual = env.lookup("graph_num")
+
+    # Assert
+    assert actual is TypeEnum.UNKNOWN, "Bindings in destroyed scope was not removed"
+
+def test_scoped_type_env_variable_shadowed():
+    # Arrange
+    expected = TypeEnum.INT
+    env = ScopedTypeEnvironment()
+
+    # Act
+    env.current_scope = env.current_scope.bind("node_num", TypeEnum.NAT)
+
+    env = env.enter_scope()
+    env.current_scope = env.current_scope.bind("node_num", TypeEnum.INT)
+
+    actual = env.lookup("node_num")
+
+    # Assert
+    assert actual is not None, "Binding not found"
+    assert actual == expected, f"actual -> {actual.name} == {expected.name} <- expected"
+
+def test_scoped_type_env_shadowed_variable_restored():
+    # Arrange
+    expected = TypeEnum.NAT
+    env = ScopedTypeEnvironment()
+
+    # Act
+    env.current_scope = env.current_scope.bind("node_num", TypeEnum.NAT)
+
+    env = env.enter_scope()
+    env.current_scope = env.current_scope.bind("node_num", TypeEnum.INT)
+
+    env = env.exit_scope()
+
+    actual = env.current_scope.lookup("node_num")
+
+    # Assert
+    assert actual is not None, "Binding not found"
+    assert actual == expected, f"actual -> {actual.name} == {expected.name} <- expected"
+
+def test_scoped_type_env_static():
+    # Arrange
+    expected = TypeEnum.INT
+    env = ScopedTypeEnvironment()
+
+    # Act
+    env.current_scope = env.current_scope.bind("node_num", TypeEnum.NAT)
+
+    new_env = env.enter_scope()
+    new_env.current_scope = new_env.current_scope.bind("node_num", TypeEnum.INT)
+
+    env = new_env.exit_scope()
+
+    actual = new_env.current_scope.lookup("node_num")
+
+    # Assert
+    assert actual is not None, "Binding not found"
+    assert actual == expected, f"actual -> {actual.name} == {expected.name} <- expected"
--- typesystem/type_env.py
+++ typesystem/type_env.py
@@ -1,61 +1,80 @@
+from typing import Any
+
 from typesystem.data_types import TypeEnum
 
 class TypeEnv():
 
     """
-    Symbol table implementation for the type environment in the type system
-
-    **Example**
-
-    ```python
-        env = TypeEnv()
-        env = env.bind("test", TypeEnum.INT)
-
-        identifier = env.lookup("test")
-        if identifer is not None:
-            print(f"Found binding: {identifier}")
-        else:
-            print(f"Binding not found: {identifier}")
-    ```
+    Base class for the type environments
     """
 
-    def __init__(self, outer_scope: "TypeEnv | None" = None) -> None:
+    def __init__(self) -> None:
         """The empty function of a symbol table"""
 
-        self.environment = dict()
-        self.outer_scope = outer_scope
+        self.environment: dict[str, Any] = dict()
 
-    def bind(self, identifier, value) -> "TypeEnv":
+    def bind(self, identifier: str, value: Any) -> "TypeEnv":
         """Binds an identifier to a type"""
 
         # To match the formal type system the typeenv should be immutable
         # To achieve this with a mutable object like dict,
         # a copy of the current scope's environment is created, updated and returned
-        # changing outer scopes never happen, so is not coppied
-        new_env = TypeEnv(outer_scope=self)
+        new_env = TypeEnv()
         new_env.environment = self.environment.copy()
         new_env.environment[identifier] = value
         return new_env
 
-    def lookup(self, identifier):
+    def lookup(self, identifier: str) -> Any:
         """Search current and outer scopes for the identifier binding"""
 
-        scope = self
-        while scope is not None:
-            if identifier in scope.environment:
-                return scope.environment[identifier]
-            scope = scope.outer_scope
+        if identifier in self.environment:
+            return self.environment[identifier]
 
         return TypeEnum.UNKNOWN
 
-    def enter_scope(self) -> "TypeEnv":
-        """Creates a new scope the references the outer scope"""
-        return TypeEnv(outer_scope=self)
+class ScopedTypeEnvironment():
 
-    def exit_scope(self) -> "TypeEnv":
+    """
+    Implements the recursive definition of the scoped type environments
+    and the two common operations for them: Enter scope and Exit scope
+    """
+
+    def __init__(self, outer_scope: "ScopedTypeEnvironment | None" = None,
+                 current_scope: TypeEnv = TypeEnv()
+                 ) -> None:
+        """The empty function of a symbol table"""
+
+        self.current_scope: TypeEnv = current_scope
+        self.outer_scope: "ScopedTypeEnvironment | None" = outer_scope
+
+    def lookup(self, identifier: str) -> Any:
+        """Search the current scope and the outer scopes for the identifier binding"""
+
+        lookup_type = self.current_scope.lookup(identifier)
+
+        if lookup_type == TypeEnum.UNKNOWN: # identifier not found in the current scope
+            lookup_type = self.outer_scope.lookup(identifier) if self.outer_scope else TypeEnum.UNKNOWN
+
+        return lookup_type
+
+    def enter_scope(self) -> "ScopedTypeEnvironment":
+        """Creates a new scope the references the outer scope"""
+
+        return ScopedTypeEnvironment(outer_scope=self)
+
+    def exit_scope(self) -> "ScopedTypeEnvironment":
         """Exits the current scope if it is not the outermost"""
 
         if self.outer_scope is None:
-            return self
+            raise Exception("No scope to exit")
 
         return self.outer_scope
+
+class VariableEnv(TypeEnv):
+    pass
+
+class AlgorithmEnv(TypeEnv):
+    pass
+
+class GraphEnv(TypeEnv):
+    pass
