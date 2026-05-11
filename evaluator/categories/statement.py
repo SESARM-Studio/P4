a8118b7bdc662eeac1bad24de7b6eb93693fdf06
SorenAAU <sesc24@student.aau.dk>
2026-05-11 09:37:15 +0200

    Removed one of the two declarations

--- evaluator/categories/statement.py
+++ evaluator/categories/statement.py
@@ -28,8 +28,12 @@ def execute_statement(node: ASTNode, loc, graph_object, store, env_var, env_algo
         case Declaration():
             D = evaluator.categories.declaration.execute_declaration(node, env_graph, env_var, env_algo, loc, graph_object, store)
             return D.store, D.env_var, env_algo, env_graph, None, D.location
+
         case Assignment():
+            if len(node.identifiers) > 1:
+                raise RuntimeError("Can only assign one identifier at a time")
             # ass
+
             print("Hejsa")
             #list-ass
 
@@ -37,6 +41,7 @@ def execute_statement(node: ASTNode, loc, graph_object, store, env_var, env_algo
             print(node)
             #if node.children[0].token == 'ArrayAccess':
             #    print(node.children[0].children[0].token, "Array")
+
         case IfStatement():
             copy_store = deepcopy(store)
             expression, copy_store = evaluator.categories.expression.execute_expression(node.condition, loc, graph_object, store, env_var, env_algo, env_graph) 
@@ -66,20 +71,20 @@ def execute_statement(node: ASTNode, loc, graph_object, store, env_var, env_algo
                     return store, env_var, env_algo, env_graph, None, loc
         case ForEachNormal():
             pass
+
         case ForEachEdge():
             pass
+
         case RepeatStatement():
             pass
+
         case Algorithm():
             ph_env_algo = evaluator.categories.algorithm.execute_algorithm(node, env_graph, env_var, env_algo, loc, graph_object, store)
             return store, env_var, ph_env_algo, env_graph, None, loc
         
-        case Expression():
-            exp_store = evaluator.categories.expression.execute_expression(node, env_graph, env_var, env_algo, loc, graph_object, store)
-            return store, env_var, env_algo, env_graph, None, loc
-        
         case GraphDecl():
             pass
+
         case Expression():
             v,exp_store = evaluator.categories.expression.execute_expression(node, env_graph, env_var, env_algo, loc, graph_object, store)
             return exp_store, env_var, env_algo, env_graph, None, loc
@@ -93,12 +98,15 @@ def execute_statement(node: ASTNode, loc, graph_object, store, env_var, env_algo
 
         case GraphStatement():
             pass
+
         case LoopModifier():
             pass
+
         case DisplayStatement():
             v,exp_store = evaluator.categories.expression.execute_expression(node.expression, env_graph, env_var, env_algo, loc, graph_object, store)
             print(v)
             return exp_store, env_var, env_algo, env_graph, None, loc
+
         case ReturnStatement():
             v,exp_store = evaluator.categories.expression.execute_expression(node.expression, env_graph, env_var, env_algo, loc, graph_object, store)
             return exp_store, env_var, env_algo, env_graph, v, loc
