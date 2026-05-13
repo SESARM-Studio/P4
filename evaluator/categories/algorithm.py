bacc8e9f48115d80865b5a3358a3ed9970fa8e98
Zhdun12 <spongo123456@gmail.com>
2026-05-13 15:32:18 +0200

    Fixed env_var is only shallow copy

--- evaluator/categories/algorithm.py
+++ evaluator/categories/algorithm.py
@@ -4,7 +4,7 @@ from parser.ast_builder import *
 def execute_algorithm(node: Algorithm, env_graph, env_var, env_algo, loc, graph_object, store):
     match node.token:
         case "Algorithm":
-            env_graph_cpy = deepcopy(env_graph)
+            env_graph_cpy = env_graph.copy()
             env_var_cpy = deepcopy(env_var)
             env_algo_cpy = deepcopy(env_algo)
             env_algo.update({node.identifier: (
