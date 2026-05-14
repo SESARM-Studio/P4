b5bf3135d38e5916cd5538082522daf02ca7504b
Zhdun12 <spongo123456@gmail.com>
2026-05-14 14:42:48 +0200

    Tests for expressions done (covering all cases)

--- tests/evaluator_unit_tests/test_expression.py
+++ tests/evaluator_unit_tests/test_expression.py
@@ -1,7 +1,10 @@
 from parser.ast_builder import *
 from evaluator.functions import *
-import evaluator.categories.expression
 from copy import deepcopy
+
+import evaluator.categories.expression
+import evaluator.categories.statement
+
 
 ### AST tree helpers ###
 def make_term(_type=None, value=None, children=None, token="Term"):
@@ -203,7 +206,7 @@ def test_expression_or1():
     arg1 = make_term("BOOL_VALUE", "true")
     arg2 = make_term("BOOL_VALUE", "false")
 
-    expression = make_expression("and", arg1, arg2)
+    expression = make_expression("or", arg1, arg2)
 
     env_graph = dict()
     env_var = dict()
@@ -223,7 +226,7 @@ def test_expression_or2():
     arg1 = make_term("BOOL_VALUE", "false")
     arg2 = make_term("BOOL_VALUE", "false")
 
-    expression = make_expression("and", arg1, arg2)
+    expression = make_expression("or", arg1, arg2)
 
     env_graph = dict()
     env_var = dict()
@@ -597,7 +600,7 @@ def test_expression_geq2():
     arg1 = make_term("IDENTIFIER", "a")
     arg2 = make_term("IDENTIFIER", "b")
 
-    expression = make_expression("<=", arg1, arg2)
+    expression = make_expression(">=", arg1, arg2)
 
     env_graph = dict()
     env_var = dict()
@@ -626,7 +629,7 @@ def test_expression_geq3():
     arg1 = make_term("IDENTIFIER", "a")
     arg2 = make_term("IDENTIFIER", "b")
 
-    expression = make_expression("<=", arg1, arg2)
+    expression = make_expression(">=", arg1, arg2)
 
     env_graph = dict()
     env_var = dict()
@@ -715,7 +718,7 @@ def test_expression_mul():
 
 def test_expression_div():
     # Arrange
-    arg1 = make_term("INTEGER_NUMBER", "-24.5")
+    arg1 = make_term("REAL_NUMBER", "-24.5")
     arg2 = make_term("INTEGER_NUMBER", "7")
 
     expression = make_expression("/", arg1, arg2)
@@ -831,7 +834,7 @@ def test_expression_abs():
     exp = Expression("Expression")
     exp.arg1 = arg1
 
-    abs_expression = AbsoluteValue()
+    abs_expression = AbsoluteValue("AbsoluteValue")
     abs_expression.expression = exp
 
     env_graph = dict()
@@ -854,7 +857,7 @@ def test_expression_mag1():
     exp = Expression("Expression")
     exp.arg1 = arg1
 
-    abs_expression = Magnitude()
+    abs_expression = Magnitude("Magnitude")
     abs_expression.expression = exp
 
     env_graph = dict()
@@ -877,7 +880,7 @@ def test_expression_mag2():
     exp = Expression("Expression")
     exp.arg1 = arg1
 
-    abs_expression = Magnitude()
+    abs_expression = Magnitude("Magnitude")
     abs_expression.expression = exp
 
     env_graph = dict()
@@ -917,19 +920,24 @@ def test_expression_cll():
     arg2 = make_term("IDENTIFIER", "b")
     body_expression = make_expression("+", arg1, arg2)
 
-    body_statement = ReturnStatement()
+    body_statement = ReturnStatement("ReturnStatement")
     body_statement.expression = body_expression
 
     body_statements = [body_statement]
 
     # Algorithm parameters
-    par1 = make_term("IDENTIFIER", "a")
-    par2 = make_term("IDENTIFIER", "b")
+    par1 = Parameter("Parameter")
+    par1.identifier = "a"
+    par1.type = "nat"
+
+    par2 = Parameter("Parameter")
+    par2.identifier = "b"
+    par2.type = "nat"
+
     parameters = [par1, par2]
 
     # Update algorithm environment for algorithm identifier
-    algorithm_identifier = make_term("IDENTIFIER", "f")
-    env_algo.update({algorithm_identifier: (parameters, body_statements, env_graph.copy(), deepcopy(env_var), deepcopy(env_algo))})
+    env_algo.update({"f": (parameters, body_statements, env_graph.copy(), deepcopy(env_var), deepcopy(env_algo))})
 
 
     # Algorithm call arguments
@@ -942,7 +950,7 @@ def test_expression_cll():
     exp2.arg1 = arg2
 
     # Algorithm call node
-    algorithm_call = AlgorithmCall()
+    algorithm_call = AlgorithmCall("AlgorithmCall")
     algorithm_call.arguments = [exp1, exp2]
     algorithm_call.identifier = "f"
 
@@ -958,13 +966,24 @@ def test_expression_cll():
 
 def test_expression_list():
     # Arrange
-    arg1 = make_term("IDENTIFIER", "a")
+    exp_inner1 = make_expression(arg1=make_term("NATURAL_NUMBER", "1"))
+    exp_inner2 = make_expression(arg1=make_term("NATURAL_NUMBER", "2"))
+    exp_inner3 = make_expression(arg1=make_term("NATURAL_NUMBER", "3"))
+    exp_inner4 = make_expression(arg1=make_term("NATURAL_NUMBER", "4"))
 
-    exp = Expression("Expression")
-    exp.arg1 = arg1
+    exp_list_inner1 = ListExpression("ListExpression")
+    exp_list_inner1.expressions = [exp_inner1, exp_inner2]
 
-    abs_expression = Magnitude()
-    abs_expression.expression = exp
+    exp_list_inner2 = ListExpression("ListExpression")
+    exp_list_inner2.expressions = [exp_inner3, exp_inner4]
+
+    exp_outer1 = make_expression(arg1=exp_list_inner1)
+    exp_outer2 = make_expression(arg1=exp_list_inner2)
+
+    exp_list_outer = ListExpression("ListExpression")
+    exp_list_outer.expressions = [exp_outer1, exp_outer2]
+
+    exp_main = make_expression(arg1=exp_list_outer)
 
     env_graph = dict()
     env_var = dict()
@@ -973,17 +992,129 @@ def test_expression_list():
     graph_object = Graph()
     store = dict()
 
-    env_var.update({"a": loc})
-    store.update({loc: [1,2,3]})
-
     # Act
-    v1, store1 = evaluator.categories.expression.execute_expression(abs_expression, env_graph, env_var, env_algo, loc.next_location(), graph_object, store)
+    v1, store1 = evaluator.categories.expression.execute_expression(exp_main, env_graph, env_var, env_algo, loc.next_location(), graph_object, store)
 
     # Assert
-    assert v1 == 3
+    assert v1 == [[1,2],[3,4]]
 
 
+# ********* Indexing *********
+
+def test_expression_indexing():
+    # Arrange
+    env_graph = dict()
+    env_var = dict()
+    env_algo = dict()
+    loc = Location()
+    graph_object = Graph()
+    store = dict()
+
+    env_var.update({"a": loc})
+    store.update({loc: [[1,2],[3,4]]})
+
+    exp_inner1 = make_expression(arg1=make_term("NATURAL_NUMBER", "1"))
+    exp_inner2 = make_expression(arg1=make_term("NATURAL_NUMBER", "2"))
+
+    array_access_node = ArrayAccess("ArrayAccess")
+    array_access_node.identifier = "a"
+    array_access_node.indexes = [exp_inner1, exp_inner2]
+
+    exp_outer = make_expression(arg1=array_access_node)
+
+    # Act
+    v1, store1 = evaluator.categories.expression.execute_expression(exp_outer, env_graph, env_var, env_algo, loc.next_location(), graph_object, store)
+
+    # Assert
+    assert v1 == 2
 
 
+# ********* Weight of edges *********
+
+def test_expression_woe1():
+
+    # Arrange
+    env_graph = dict()
+    env_var = dict()
+    env_algo = dict()
+    loc = Location()
+    graph_object = Graph()
+    store = dict()
+
+    # Arrange for declaring graph G with nodes a,b and the edge a-->b with weight 5 (nat)
+    exp_weight = make_expression(arg1=make_term("NATURAL_NUMBER", "5"))
+
+    decl_edge = EdgeDecl("EdgeDecl")
+    decl_edge.initial_node = 'a'
+    decl_edge.nodes = ['b']
+    decl_edge.direction = "-->"
+    decl_edge.weight = [exp_weight]
+
+    decl_nodes = NodeDecl("NodeDecl")
+    decl_nodes.type = "node"
+    decl_nodes.identifiers = ['a', 'b']
+    decl_nodes.is_list = False
+
+    decl_graph = GraphDecl("GraphDecl")
+    decl_graph.graph_type = "digraph"
+    decl_graph.identifier = "G"
+    decl_graph.weight_type = "nat"
+    decl_graph.nodes = [decl_nodes]
+    decl_graph.edges = [decl_edge]
+
+    # Declaring the graph G
+    store, env_var, env_algo, env_graph, v, loc = evaluator.categories.statement.execute_statement(decl_graph, loc, graph_object, store, env_var, env_algo, env_graph)
+
+    # Arranging for weight of node
+    exp = make_expression("weight of", arg1="G.a-->b")
+
+    # Act
+    v1, store1 = evaluator.categories.expression.execute_expression(exp, env_graph, env_var, env_algo, loc.next_location(), graph_object, store)
+
+    # Assert
+    assert v1 == 5
+
+def test_expression_woe2():
+
+    # Arrange
+    env_graph = dict()
+    env_var = dict()
+    env_algo = dict()
+    loc = Location()
+    graph_object = Graph()
+    store = dict()
+
+    # Arrange for declaring graph G with nodes a,b and the edge a-->b with weight 5 (nat)
+    exp_weight = make_expression(arg1=make_term("NATURAL_NUMBER", "5"))
+
+    decl_edge = EdgeDecl("EdgeDecl")
+    decl_edge.initial_node = 'a'
+    decl_edge.nodes = ['b']
+    decl_edge.direction = "---"
+    decl_edge.weight = [exp_weight]
+
+    decl_nodes = NodeDecl("NodeDecl")
+    decl_nodes.type = "node"
+    decl_nodes.identifiers = ['a', 'b']
+    decl_nodes.is_list = False
+
+    decl_graph = GraphDecl("GraphDecl")
+    decl_graph.graph_type = "graph"
+    decl_graph.identifier = "G"
+    decl_graph.weight_type = "nat"
+    decl_graph.nodes = [decl_nodes]
+    decl_graph.edges = [decl_edge]
+
+    # Declaring the graph G
+    store, env_var, env_algo, env_graph, v, loc = evaluator.categories.statement.execute_statement(decl_graph, loc, graph_object, store, env_var, env_algo, env_graph)
+
+    # Arranging for weight of node
+    exp = make_expression("weight of", arg1="G.a-->b")
+
+    # Act
+    v1, store1 = evaluator.categories.expression.execute_expression(exp, env_graph, env_var, env_algo, loc.next_location(), graph_object, store)
+
+    # Assert
+    assert v1 == 5
 
 
