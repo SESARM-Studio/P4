8d48f23150597469599556987a76286f21baf6ee
SorenAAU <sesc24@student.aau.dk>
2026-05-16 12:48:20 +0200

    Opdelt declaration, dimension og list_declaration i 3 filer, for at følge de semantiske grupperinger. Oprettet test til declaration

---
+++ evaluator/categories/dimension.py
@@ -0,0 +1,14 @@
+from parser.ast_builder import *
+from evaluator.functions import *
+from evaluator.categories.expression import execute_expression
+
+def execute_dimension(node: Declaration, env_graph: dict, env_var: dict, env_algo: dict, loc: Location, graph_object: Graph, store: dict):
+    # dDim
+    v, _ = execute_expression(node.dimension, env_graph, env_var, env_algo, loc, graph_object, store)
+    if v != 0:
+        a = create_dimensional_array(v)
+        return a
+
+    else:
+        raise RuntimeError("Dimension cannot be 0")
+
