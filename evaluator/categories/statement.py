5bea98163d46910f665e87c29f081cb7d926878d
Zhdun12 <spongo123456@gmail.com>
2026-05-21 21:45:26 +0200

    Fixed correct parameter passin to algorithm declaration

--- evaluator/categories/statement.py
+++ evaluator/categories/statement.py
@@ -336,7 +336,7 @@ def execute_statement(node: ASTNode, loc, graph_object, store, env_var, env_algo
                 return E.modified_store, env_var, env_algo, env_graph, v2, loc
 
         case Algorithm():
-            ph_env_algo = evaluator.categories.algorithm.execute_algorithm(node, env_graph, env_var, env_algo, loc, graph_object, store)
+            ph_env_algo = evaluator.categories.algorithm.execute_algorithm(node, env_graph, env_var, env_algo)
             return store, env_var, ph_env_algo, env_graph, None, loc
         
         case GraphDecl():
