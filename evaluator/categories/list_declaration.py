383fc239a315bbeaa44e8ef23feaa44f239979b3
SorenAAU <sesc24@student.aau.dk>
2026-05-22 10:05:22 +0200

    Fixed PR comments

--- evaluator/categories/list_declaration.py
+++ evaluator/categories/list_declaration.py
@@ -2,6 +2,7 @@ from parser.ast_builder import *
 from evaluator.functions import *
 from evaluator.categories.dimension import execute_dimension
 from typing import NamedTuple
+from copy import deepcopy
 
 class DeclarationReturn(NamedTuple):
     env_var: dict
@@ -10,29 +11,14 @@ class DeclarationReturn(NamedTuple):
 
 
 def execute_list_declaration(node: Declaration, env_graph: dict, env_var: dict, env_algo: dict, loc: Location, graph_object: Graph, store: dict) -> DeclarationReturn:
-    if len(node.identifiers) > 1: # Redundant? Possibly
-        raise RuntimeError("Can only declare one array at a time")
-
     env_graph_copy = env_graph.copy()
-    loc = loc.copy()
-    env_var_copy = env_var.copy()
-    env_algo_copy = env_algo.copy()
-    store_copy = store.copy()
+    env_var_copy = deepcopy(env_var)
+    env_algo_copy = deepcopy(env_algo)
 
-    if node.dimension is None:
-        # dList2 I list in T
-        a = create_dimensional_array(1)
-        env_var_copy.update({node.identifiers[0]: loc})
-        store_copy.update({loc: a})
-        loc = loc.next_location()
+    # DLIST
+    array = execute_dimension(node, env_graph_copy, env_var_copy, env_algo_copy, loc, graph_object, store)
+    env_var_copy.update({node.identifiers[0]: loc})
+    store.update({loc: array})
+    loc = loc.next_location()
 
-        return DeclarationReturn(env_var_copy, store_copy, loc)
-
-    else:
-        # dList1 I DIM list in T
-        a = execute_dimension(node, env_graph_copy, env_var_copy, env_algo_copy, loc, graph_object, store_copy)
-        env_var_copy.update({node.identifiers[0]: loc})
-        store_copy.update({loc: a})
-        loc = loc.next_location()
-
-        return DeclarationReturn(env_var_copy, store_copy, loc)
+    return DeclarationReturn(env_var_copy, store, loc)
