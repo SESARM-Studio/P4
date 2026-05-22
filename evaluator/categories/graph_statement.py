64f5fa031b2e88e74822234cd37f429c0da1a4b5
MartinHZ <mzacha22@student.aau.dk>
2026-05-22 03:08:50 +0200

    Fixed PR comments about graph_statement

--- evaluator/categories/graph_statement.py
+++ evaluator/categories/graph_statement.py
@@ -6,41 +6,51 @@ def execute_graph_statement(tree_node: GraphStatement, env_graph, env_var, env_a
     match tree_node.operator:
         case "add":
             match tree_node.argument:
-                case EdgeDecl():
+                case Declaration():
+                    # Dont need to parse this to execute_declaration because we are not declaring new nodes.
+                    if not env_var.get(tree_node.argument.identifiers[0]): # (ADD-NODE1)
+                        if isinstance(env_graph.get(tree_node.graph_identifier), Graph):
+                            g = env_graph.get(tree_node.graph_identifier)
+                            g.add_node(tree_node.argument.identifiers[0])
+                        else:
+                            print("Cant add node to not graph object")
+                    else:
+                        location = env_var.get(tree_node.argument.identifiers[0])
+                        value = store.get(location)
+                        if isinstance(env_graph.get(tree_node.graph_identifier), Graph):
+                            g = env_graph.get(tree_node.graph_identifier)
+                            g.add_node(value)
+                        else:
+                            print("Cant add node to not graph object")
+
+                case EdgeDecl(): # (ADD-EDGE)
                     if isinstance(env_graph.get(tree_node.graph_identifier), Graph):
-                        graph = env_graph.get(tree_node.graph_identifier)
+                        g = env_graph.get(tree_node.graph_identifier)
                         if tree_node.argument.weight != []:
                             edge, store = evaluator.categories.edge_declaration.execute_edge_declaration(tree_node.argument, env_graph, env_var, env_algo, loc, graph_object=Graph(), store = store)
-                            graph.add_weighted_edges(edge)
+                            g.add_weighted_edges(edge)
                         else:
                             edge, store = evaluator.categories.edge_declaration.execute_edge_declaration(tree_node.argument, env_graph, env_var, env_algo, loc, graph_object=Graph(), store = store)
-                            graph.add_edges(edge)
+                            g.add_edges(edge)
                     else:
                         print("Cant add edge to not graph object")
 
-                case Declaration():
-                    # Dont need to parse this to executre_declaration because we are only interested in the identifier.
-                    if isinstance(env_graph.get(tree_node.graph_identifier), Graph):
-                        graph = env_graph.get(tree_node.graph_identifier)
-                        graph.add_node(tree_node.argument.identifiers[0])
-                    else:
-                        print("Cant add node to not graph object")
-
         case "remove":
             match tree_node.argument:
-                case EdgeDecl():
+                case Declaration(): # (REMOVE-NODE)
+                    # Dont need to parse this to executre_declaration because we are only interested in the identifier.
                     if isinstance(env_graph.get(tree_node.graph_identifier), Graph):
-                        graph = env_graph.get(tree_node.graph_identifier)
-                        edges_to_remove, store = evaluator.categories.edge_declaration.execute_edge_declaration(tree_node.argument, env_graph, env_var, env_algo, loc, graph_object=Graph(), store = store)
-                        for edge in edges_to_remove:
-                            graph.remove_edge(edge[0], edge[1])
+                        g = env_graph.get(tree_node.graph_identifier)
+                        g.remove_node(tree_node.argument.identifiers[0])
+                    else:
+                        print("Can't remove node from not graph object")
+
+                case EdgeDecl(): # (REMOVE-EDGE)
+                    if isinstance(env_graph.get(tree_node.graph_identifier), Graph):
+                        g = env_graph.get(tree_node.graph_identifier)
+                        edge_to_remove, store = evaluator.categories.edge_declaration.execute_edge_declaration(tree_node.argument, env_graph, env_var, env_algo, loc, graph_object=Graph(), store = store)
+                        g.remove_edge(edge_to_remove[0][0], edge_to_remove[0][1]) #edge_to_remove is an array of a tuple e.g. [("a", "b")] so [0][0] = "a" [0][1] = "b"
                     else:
                         print("Cant remove edge from not graph object")
 
-                case Declaration():
-                    # Dont need to parse this to executre_declaration because we are only interested in the identifier.
-                    if isinstance(env_graph.get(tree_node.graph_identifier), Graph):
-                        graph = env_graph.get(tree_node.graph_identifier)
-                        graph.remove_node(tree_node.argument.identifiers[0])
-                    else:
-                        print("Can't remove node from not graph object")
+                
