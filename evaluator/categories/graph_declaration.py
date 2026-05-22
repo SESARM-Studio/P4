bc8990021686deb00e1c6022fb4044e3b11efab8
MartinHZ <mzacha22@student.aau.dk>
2026-05-22 03:08:32 +0200

    Fixed PR comments about graph_declaration.

--- evaluator/categories/graph_declaration.py
+++ evaluator/categories/graph_declaration.py
@@ -4,23 +4,17 @@ import evaluator.categories.declaration
 import evaluator.categories.edge_declaration
 
 def execute_graph_decl(tree_node: GraphDecl, env_var, env_algo, loc, env_graph, graph_object: Graph, store):
-    if tree_node.weight_type is not None:
-        if tree_node.nodes != [] or tree_node.edges != []:
+    if tree_node.nodes == [] and tree_node.edges == []:
+        if tree_node.weight_type is None: # (DGRAPH1)
             graph_object = Graph()
             graph_object.create_graph(tree_node.graph_type)
-            
-            for node in tree_node.nodes:
-                evaluator.categories.declaration.execute_declaration(node, env_graph, env_var, env_algo, loc, graph_object, store)
-            
-            for edge in tree_node.edges:
-                evaluator.categories.edge_declaration.execute_edge_declaration(edge, env_graph, env_var, env_algo, loc, graph_object, store)
-            
+
             env_graph.update({tree_node.identifier: graph_object})
 
             graph_object = Graph()
 
             return env_graph, store
-        else:
+        else: # (DGRAPH2)
             graph_object = Graph()
             graph_object.create_graph(tree_node.graph_type)
 
@@ -30,7 +24,7 @@ def execute_graph_decl(tree_node: GraphDecl, env_var, env_algo, loc, env_graph, 
 
             return env_graph, store
     else:
-        if tree_node.nodes != [] or tree_node.edges != []:
+        if tree_node.weight_type is None: # (DGRAPH3)
             graph_object = Graph()
             graph_object.create_graph(tree_node.graph_type)
             
@@ -45,13 +39,18 @@ def execute_graph_decl(tree_node: GraphDecl, env_var, env_algo, loc, env_graph, 
             graph_object = Graph()
 
             return env_graph, store
-        else:
+        else: # (DGRAPH4)
             graph_object = Graph()
             graph_object.create_graph(tree_node.graph_type)
-
+            
+            for node in tree_node.nodes:
+                evaluator.categories.declaration.execute_declaration(node, env_graph, env_var, env_algo, loc, graph_object, store)
+            
+            for edge in tree_node.edges:
+                evaluator.categories.edge_declaration.execute_edge_declaration(edge, env_graph, env_var, env_algo, loc, graph_object, store)
+            
             env_graph.update({tree_node.identifier: graph_object})
 
             graph_object = Graph()
 
             return env_graph, store
-                
