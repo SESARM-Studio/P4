2cc18425086730c0ff194a978c66e3e850e35fce
Zhdun <anoshe24@student.aau.dk>
2026-05-24 21:14:43 +0200

    Corrected rule comment

--- evaluator/evaluator.py
+++ evaluator/evaluator.py
@@ -12,6 +12,6 @@ def traverse_program(program: ASTNode):
     store = dict()
 
     if len(program.children) != 1: # (PROG1)
-        for statement in program.children: # (cmp2)
+        for statement in program.children: # (PROG1) and (cmp2)
             if not statement.token == "EOF":
                 store, env_var, env_algo, env_graph, v, loc = execute_statement(statement, loc, graph_object, store, env_var, env_algo, env_graph)
